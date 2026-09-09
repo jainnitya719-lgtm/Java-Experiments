19027fbec5834baa9cb059671663457e25bd2a7f
jainnitya719-lgtm <jainnitya719@gmail.com>
2026-09-09 22:34:10 +0530

    Create Q6.java

---
+++ src/Q6.java
@@ -0,0 +1,13 @@
+class SimpleObject {
+
+    SimpleObject() {
+        System.out.println("Constructor is called");
+        System.out.println("Object created successfully");
+    }
+}
+
+public class Q6 {
+    public static void main(String[] args) {
+        SimpleObject obj = new SimpleObject();
+    }
+}
