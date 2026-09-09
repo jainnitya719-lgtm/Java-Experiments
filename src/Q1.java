039896db43a8d73cdbb1b32267d17c7e1893919b
jainnitya719-lgtm <jainnitya719@gmail.com>
2026-09-09 22:31:20 +0530

    Create Q1.java

---
+++ src/Q1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 3 numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int greater = a;
+
+        if (b > greater)
+            greater = b;
+
+        if (c > greater)
+            greater = c;
+
+        System.out.println("Greater number = " + greater);
+    }
+}
