cdbd6d495aa3db4e7c8f5087223ff04a03523617
jainnitya719-lgtm <jainnitya719@gmail.com>
2026-09-09 22:32:12 +0530

    Create Q2.java

---
+++ src/Q2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int count = 0;
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                count++;
+                sum += i;
+            }
+        }
+
+        System.out.println("Total odd numbers = " + count);
+        System.out.println("Sum = " + sum);
+    }
+
+}
